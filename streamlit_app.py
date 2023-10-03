73c0de9a7fdf1bd260f7a7a13091bc17254be8d8
Shirsh Mall <87264071+shirsh10mall@users.noreply.github.com>
2023-10-04 01:55:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,21 +11,21 @@ def show_predict_page():
     st.write("""# Patient Survival Prediction App""")
     st.write("""### Enter information for prediction """)        
     
-    gcs_verbal_apache = st.selectbox('Product Group ', options=[1.0,2.0,3.0,4.0,5.0] )
-    gcs_motor_apache = st.selectbox('Product Content ', options=[1.0,2.0,3.0,4.0,5.0,6.0] )
-    ventilated_apache = st.selectbox('Unit ', options=['yes', 'no'] )
+    gcs_verbal_apache = st.selectbox('GCS Verbal Apache', options=[1.0,2.0,3.0,4.0,5.0] )
+    gcs_motor_apache = st.selectbox('GCS Motor Apache', options=[1.0,2.0,3.0,4.0,5.0,6.0] )
+    ventilated_apache = st.selectbox('Ventilated Apache ', options=['yes', 'no'] )
     if ventilated_apache == "yes":
         ventilated_apache = 1.0
     else:
         ventilated_apache = 0.0
 
-    gcs_eyes_apache = st.selectbox('Dosage Form ', options=[1.0,2.0,3.0,4.0] )
+    gcs_eyes_apache = st.selectbox('GCS Eyes Apache', options=[1.0,2.0,3.0,4.0] )
 
 
-    apache_4a_hospital_death_prob = st.slider( 'Apache Hospital Death Probaboloty', 0.01, 0.99, 0.01 )
-    apache_4a_icu_death_prob = st.slider( 'Lowest Competitor Price ', 0.01, 0.99, 0.01)
-    d1_sysbp_noninvasive_min = st.slider( 'Product Price', 41.0, 160.0, 1.0 )
-    d1_sysbp_min = st.slider( 'Manufacrurer ID ', 41.0, 160.0, 1.0 )
+    apache_4a_hospital_death_prob = st.slider( 'Apache Hospital Death Probability', 0.01, 0.99, 0.01 )
+    apache_4a_icu_death_prob = st.slider( 'Apache ICU Death Probability', 0.01, 0.99, 0.01)
+    d1_sysbp_noninvasive_min = st.slider( 'SysBP NonInvassive Min', 41.0, 160.0, 1.0 )
+    d1_sysbp_min = st.slider( 'SysBP Min ', 41.0, 160.0, 1.0 )
     
     ok = st.button("Predict")
     
